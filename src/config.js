6b74bbfcaca5b4426e73e6417fe2429c02ce29f5
Bekele <simeneh@amazon.com>
2020-05-06 16:26:14 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "musiccatalog-janmusic"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://vqt13dyt8l.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_8NGCtPhC6",
+      APP_CLIENT_ID: "36vfha489ofnkb1edbih6jf24p",
+      IDENTITY_POOL_ID: "us-east-1:7e56c721-9db0-488c-9d5b-7ac6529da060"
+    }
+  };
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Music Catalog</h1>
+        <p>A simple Music catalog registration app</p>
+      </div>
+    </div>
+  );
+}
